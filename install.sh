3d77ad0237f089f8e44dba46c89b0fa8856112c2
muhit-emon <82660145+muhit-emon@users.noreply.github.com>
2023-08-25 02:23:30 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,10 @@ tar xzf diamond-linux64.tar.gz
 chmod +x diamond
 rm diamond-linux64.tar.gz
 
+# prodigal installation
+wget https://github.com/hyattpd/Prodigal/releases/download/v2.6.3/prodigal.linux
+mv prodigal.linux prodigal
+chmod +x prodigal
+
 # conda environment installation
 conda env create -f environment.yml
