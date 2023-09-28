35f2215ca62d7a3c49a3c5defde4f71d7ae06771
muhit-emon <82660145+muhit-emon@users.noreply.github.com>
2023-09-28 01:36:04 -0400

    Update run_diamond_against_ref_exp.sh

--- run_diamond_against_ref_exp.sh
+++ run_diamond_against_ref_exp.sh
@@ -9,4 +9,4 @@
 ref_db=$2/ref_exp
 
 chmod +x $2/diamond
-$2/diamond blastp -q "$1" -d ${ref_db} -o diamond_with_ref_exp.tsv --id 20 --evalue 1e-7 -k 485 --query-cover 60 -f 6 qtitle stitle pident bitscore evalue qlen slen qstart qend sstart send --very-sensitive --quiet
+$2/diamond blastp -q "$1" -d ${ref_db} -o diamond_with_ref_exp.tsv --id 20 --evalue 1e-7 -k 485 --query-cover 60 --subject-cover 60 -f 6 qtitle stitle pident bitscore evalue qlen slen qstart qend sstart send --very-sensitive --quiet
